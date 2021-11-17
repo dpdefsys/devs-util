c526b7b61bb7ad23a5bc29fe003622b569382031
dpdefsys <deep@defsysgroup.com>
2021-11-17 13:12:56 +0530

    added methods

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # devs-util
 devs-util
+
+## Installation
+
+run `npm install devs-util`
+
+## Use
+
+to use method that fetches users
+```
+import { GetUsers } from 'devs-util'
+
+GetUsers().then(console.log)
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export function GetUsers() {
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => resolve(response.json()))
+      .then((json) => reject(json));
+  });
+}
+
+export function GetUserById(id: string) {
+  if (!id) {
+    throw new Error('Id missing');
+  }
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/users/' + id)
+      .then((response) => resolve(response.json()))
+      .then((json) => reject(json));
+  });
+}
+
+export function GetPosts() {
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => resolve(response.json()))
+      .then((json) => reject(json));
+  });
+}
+
+export function GetPostById(id: string) {
+  if (!id) {
+    throw new Error('Id missing');
+  }
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then((response) => resolve(response.json()))
+      .then((json) => reject(json));
+  });
+}
